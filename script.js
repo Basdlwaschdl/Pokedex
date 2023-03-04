256499b67cbe13e0590365eca1f081c57d85ef52
Basdlwaschdl <ftpobi@googlemail.com>
2023-03-04 23:35:18 +0100

    final commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let types = ['normal', 'fire', 'water', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug', 'ghost', 'steel', 'dragon', 'dark', 'fairy'];
 let colors = ['#A8A77A', '#EE8130', '#6390F0', '#7AC74C', '#A98FF3', '#C22E28', '#A33EA1', '#F7D02C', '#E2BF65', '#B6A136', '#F95587', '#96D9D6', '#A6B91A', '#735797', '#B7B7CE', '#6F35FC', '#705746', '#D685AD'];
+let pokemon_Names = [];
 let pokemonImg;
 let pokemonName;
 let pokemonType;
@@ -21,6 +22,17 @@ async function loadAllpokemon() {
         let id = pokemonJson['id'];
         let type = pokemonJson['types'][0]['type']['name'];
         renderAllPokemon(name, img, id, type);
+    }
+}
+
+
+async function loadNames_inArray() {
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
+    let namesJson = await response.json();
+    namesJson = namesJson['results'];
+    for (let i = 0; i < namesJson.length; i++) {
+        let name = namesJson[i]['name'];
+        pokemon_Names.push(name);
     }
 }
 
@@ -60,6 +72,7 @@ function renderPokemon() {
         let type = pokemonType[i]['type']['name'];
         let index = types.indexOf(type);
         let color = colors[index];
+        document.getElementById('pokemonImg').style.backgroundColor = `${color}`;
         document.getElementById('types').innerHTML += `
         <div id="typeSub${i}" style="background-color:${color};">${type}</div>`;
     }
@@ -75,7 +88,7 @@ function renderStats() {
         document.getElementById('weight').innerHTML = `${pokemonWeight} Kg<p>Weight</p>`;
         document.getElementById('height').innerHTML = `${pokemonHeight} M<p>Height</p>`;
         document.getElementById('Name').innerHTML = `${pokemonName}`;
-        document.getElementById('pokemonImg').style.backgroundImage = `url(${pokemonImg}`;
+        document.getElementById('pokemonImg').innerHTML = `<img src=${pokemonImg}>`;
         document.getElementById('backIDsub').innerHTML = `
         <img src="img/arrow-left.ico" onclick="previosPokemon(${pokemonId})">
         <stan id="backId">#${pokemonId}</stan>
@@ -100,17 +113,41 @@ function nextPokemon(pokemonId) {
 }
 
 
-function loadNextPokemons() {
+function loadNextPokemons(x) {
+    if ( x == 20){
     start += 20;
     limit += 20;
     loadAllpokemon();
+    }
+    if ( x == 100){
+        start += 100;
+        limit += 100;
+        loadAllpokemon();
+    }
+    if ( x == 'all') {
+        limit = 1009;
+        loadAllpokemon();
+    }
 }
 
 
-function searchPokemon() {
+function searchPokemon_ById() {
     let name = document.getElementById('searchById').value;
     if (name < 1 || name > 1008) {
         alert('Please enter a ID between 1 and 1008')
+    }
+    else {
+        loadPokemon(name);
+    }
+}
+
+
+function searchPokemon_ByName() {
+    let name = document.getElementById('searchName').value;
+    name = name.toLowerCase();
+    let i = pokemon_Names.indexOf(name);
+    if ( i == -1) {
+        alert(`${name} doesn't exist or you have a typo`)
     }
     else {
         loadPokemon(name);
@@ -122,4 +159,10 @@ function closePokemon() {
     document.getElementById('showPokemon').style.transform = "scale(0.0)";
     document.getElementById('allPokemonMain').style.filter = "none"
     document.getElementById('background').style.display = 'none';
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
 }
